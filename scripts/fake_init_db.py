0923a34e0fc4ce333f2f13b841e03f9d52e93c07
David Liencura <davidw.rq@gmail.com>
2021-05-04 23:48:38 -0700

    chore: refactor directories and add readme to main project

--- scripts/fake_init_db.py
+++ scripts/fake_init_db.py
@@ -3,7 +3,7 @@ from sqlfaker.database import Database
 # add database
 my_db = Database(db_name="test.db")
 
-my_db.add_table(table_name="Enterprise", n_rows=10)
+my_db.add_table(table_name="enterprise", n_rows=10)
 
 my_db.generate_data()
-my_db.export_sql("test.sql")
+my_db.export_sql("db/init.sql")
